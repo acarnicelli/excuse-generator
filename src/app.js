4caea3ded0aa5b8bb04be3cbc185431048e152ae
acarnicelli <alecarnicelli@hotmail.com>
2021-08-21 01:25:52 +0000

    Ejercicio Excuse Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,46 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = [
+  "El Power Ranger rojo ",
+  "Un imitador de Michael Jackson ",
+  "Un ornitorrinco mutante ",
+  "Mi clon "
+];
+
+let action = ["quemó ", "empeñó ", "desintegró ", "trituró "];
+
+let what = [
+  "a mi gato ",
+  "mi última empanada ",
+  "mis panchos ",
+  "mi colección de Pokemón "
+];
+
+let when = [
+  "antes de ayer.",
+  "el día de la marmota.",
+  "cuando estaba mirando Naruto.",
+  "el día de entrega del proyecto final de 4Geeks."
+];
+
+function enteroRandom(min, max) {
+  let num = Math.random() * (max - min) + min;
+  return Math.floor(num);
+}
+
+function randomString(array) {
+  let len = array.length;
+  return array[enteroRandom(0, len)];
+}
+
+let contenido =
+  randomString(who) +
+  randomString(action) +
+  randomString(what) +
+  randomString(when);
+
+excuse.innerHTML = contenido;
